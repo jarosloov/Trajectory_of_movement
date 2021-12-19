37a6be463ed027e81dd5b3aaf03983c2a3606f82
Jarosloov <jarosloov@yandex.ru>
2021-12-19 18:28:34 +0500

    Первел поиск угла Theta1 из Matlab на Python, так же добавил PDF файл с новой траекторией для тестов изменение подсчета угла, вместо tg(90) === 1.57 на tg(90) === 0

--- main.py
+++ main.py
@@ -2,12 +2,14 @@ import math
 import matplotlib.pyplot as plt
 import numpy as np
 
-L = 3
-p_lat = [0, 5, 10, 13, 16, 18, 20, 21, 21, 22, 24, 28, 32, 36, 39, 42, 43, 44]  # работающие точки
-p_lot = [5, 5, 5, 6, 8, 10, 13, 16, 22, 25, 28, 30, 31, 31, 31, 31, 31, 31]
+L = 1.5
+# работающие точки
+# x = [0, 5, 10, 13, 16, 18, 20, 21, 21, 22, 24, 28, 32, 36, 39, 42, 43, 44]
+# y = [5, 5, 5, 6, 8, 10, 13, 16, 22, 25, 28, 30, 31, 31, 31, 31, 31, 31]
 
+# Новые координаты
 x = [0, 10, 13, 16, 18, 19, 19, 20, 22, 25, 31, 34, 36, 37, 38, 40, 43, 46, 56, 59, 61, 64, 68, 70]
-y = [3,  3,  4,  6,  9, 12, 28, 32, 35, 37, 37, 35, 32, 28, 19, 16, 14, 13, 13, 14, 16, 18, 19, 19]
+y = [3, 3, 4, 6, 9, 12, 28, 32, 35, 37, 37, 35, 32, 28, 19, 16, 14, 13, 13, 14, 16, 18, 19, 19]
 
 # p_lat = [0, 5, 10, 13, 16]  # работающие точки
 # p_lot = [5, 5, 5, 6, 8]
@@ -30,16 +32,16 @@ new_fi = []
 # ------------------- Поиск точки B --------------------
 # --------------------- Вариант 1 ----------------------
 
-for i in range(len(p_lat) - 1):
-    A.append(math.sqrt((p_lat[i] - p_lat[i + 1]) ** 2))
-    B.append(math.sqrt((p_lot[i] - p_lot[i + 1]) ** 2))
+for i in range(len(x) - 1):
+    A.append(math.sqrt((x[i] - x[i + 1]) ** 2))
+    B.append(math.sqrt((y[i] - y[i + 1]) ** 2))
     if int(A[i]) == 0:
-        ksi.append(1.57)  # значение в радианах при угле в 90 градусах
+        ksi.append(1.57)  # значение в радианах при угле в 90 градусах 1,57
     else:
         ksi.append(round(math.atan(float(B[i]) / float(A[i])), 4))
 
-    x_B.append(p_lat[i] - L * math.cos(ksi[i - 1]))
-    y_B.append(p_lot[i] - L * math.sin(ksi[i - 1]))
+    x_B.append(x[i] - L * math.cos(ksi[i - 1]))
+    y_B.append(y[i] - L * math.sin(ksi[i - 1]))
 
 # ------------------- Поиск точки B --------------------
 # --------------------- Вариант 2 ----------------------
@@ -86,11 +88,11 @@ for i in range(len(p_lat) - 1):
 # --------------------- Вариант 1 ----------------------
 
 
-for i in range(len(p_lat) - 2):
+for i in range(len(x) - 2):
     AA.append(math.sqrt((x_B[i] - x_B[i + 1]) ** 2))
     BB.append(math.sqrt((y_B[i] - y_B[i + 1]) ** 2))
     if int(AA[i]) == 0:
-        ksi_2.append(1.57)  # значение в радианах при угле в 90 градусах
+        ksi_2.append(0)  # значение в радианах при угле в 90 градусах
     else:
         ksi_2.append(round(math.atan(float(BB[i]) / float(AA[i])), 4))
 
@@ -117,7 +119,7 @@ def AngleSearch(x_1, y_1, x_2, y_2):
     aLen = math.fabs(x_1 - x_2)
     bLen = math.fabs(y_1 - y_2)
     if int(aLen) == 0:
-        return 1.57
+        return 0
     else:
         return round(math.atan(bLen / aLen), 4)
 
@@ -138,12 +140,37 @@ def FragmentationArea(x_1, y_1, x_2, y_2, lenFragmentation):
         print("Ошибка")
 
 
+# ------------------------- MatLab --------------------------
+# -------------------- Вычисление Theta ---------------------
+
+
+theta1 = []
+
+
+def Theta1(theta_0, speed_end, speed, phy):
+    L1 = 3  # длина трактора
+
+    num = len(x)
+    theta1.append(theta_0)
+    dd = speed_end / num
+
+    for ii in range(1, num-2):
+        theta1.append(dd * (speed * math.tan(phy[ii - 1]))
+                      / L1 + theta1[ii - 1])
+
+    return theta1
+
+# ------------------------ Проверка -------------------------
+phy = []
+for i in range(1, len(x)):
+    phy.append(AngleSearch(x[i - 1], y[i - 1], x[i], y[i]))
+print(Theta1(0, 2, 2, phy))
+
 # ----------------- Рисование графика ------------------
 
-plt.plot(p_lat, p_lot, label='output')
-plt.plot(x_B, y_B, 'o-', label='back axle')
-# plt.plot(x_B, y_B, )
-plt.plot(x_C, y_C, 'o-', label='trailer')
+plt.plot(x, y, label='output')
+plt.plot(x_B, y_B, label='back axle')
+# plt.plot(x_C, y_C, label='trailer')
 
 plt.legend()
 plt.show()
